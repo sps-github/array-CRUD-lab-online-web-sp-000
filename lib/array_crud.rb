d8c4fc129c5081a4ca04b32a5dc5aee8f9f1b0d0
Sarah Peters <sarahpieters15@gmail.com>
2020-05-27 14:19:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
- create_an_array << "cow"
+ array = ["dog", "cat", "rabbit", "mouse"]
+ array << "cow"
 end
 
 def add_element_to_start_of_array(array, element)
